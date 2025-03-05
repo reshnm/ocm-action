fa5fef7f3aa93bb896a835c8c2159d024ed66c9e
René Schünemann <rene.schuenemann@sap.com>
2025-03-05 15:02:37 +0100

    test

--- index.js
+++ index.js
@@ -6,8 +6,20 @@ try {
     const ocmVersion = core.getInput('ocm-version');
     const baseURL = core.getInput('base-url');
 
-    
 
+    console.log(`Hello from the action!`);
+    console.log(`OCM Version: ${ocmVersion}`);
+    console.log(`Base URL: ${baseURL}`);
+    
+    // I want to get the latest release of github.com/open-component-model/ocm
+    const octokit = github.getOctokit(token);
+    octokit.repos.getLatestRelease({
+        owner: 'open-component-model',
+        repo: 'ocm'
+    }).then(response => {
+        console.log(response.data.tag_name);
+        core.setOutput('tag', response.data.tag_name);
+    });
 
 } catch (error) {
     core.setFailed(error.message);
